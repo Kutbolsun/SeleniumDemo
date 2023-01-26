1df0df8d76676f6e6565d6b3fb93751c1b69d130
Kutbolsun <kutbolsun@gmail.com>
2023-01-26 21:35:28 +0600

    feature/Swift payment

--- src/main/java/org/example/test/Test.java
+++ src/main/java/org/example/test/Test.java
@@ -1,2 +1,7 @@
-package org.example.test;public class Test {
+package org.example.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My test branch");
+    }
 }
